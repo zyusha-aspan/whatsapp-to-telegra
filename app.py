b80044b730cd075ee359eb4bb381aa49824797b6
zyusha-aspan <zyusha@gmail.com>
2025-06-09 21:13:03 +0500

    app.py

--- app.py
+++ app.py
@@ -27,6 +27,6 @@ def whatsapp_webhook():
     return {'status': 'ok'}
 
 # Render запускает сервер так
-if __name__ == '__main__'
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
